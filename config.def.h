1cb55cb539ff05de3bcc90b4625b9ffaad4f17ff
rosstimo <rosstimo@isu.edu>
2024-10-13 07:15:35 -0600

    change terminal & browser

--- config.def.h
+++ config.def.h
@@ -1,9 +1,9 @@
 /* See LICENSE file for copyright and license details. */
 
 /* Constants */
-#define TERMINAL "st"
-#define TERMCLASS "St"
-#define BROWSER "librewolf"
+#define TERMINAL "alacritty"
+#define TERMCLASS "alacritty"
+#define BROWSER "firefox"
 
 /* appearance */
 static unsigned int borderpx  = 3;        /* border pixel of windows */
